46df05f2d090893ba5ce963bca1f4034ceaad905
Radoslav Stankov <rstankov@gmail.com>
2018-03-31 17:25:29 +0300

    Update seed

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -8,4 +8,10 @@ account = Account.find_by_name('CoinBox') || Account.create!(
 
 demo = account.games.find_or_create_by! name: 'Demo'
 
-api_key = demo.api_keys.find_or_create_by! name: 'Mobile'
+api_key = demo.api_keys.find_or_create_by! name: 'iOS'
+
+c1 = demo.consumables.find_by(identifier: 'exp') || demo.consumables.create!(identifier: 'exp', name: 'Expirience', value: 0, tradeable: false)
+c2 = demo.consumables.find_by(identifier: 'coin') || demo.consumables.create!(identifier: 'coin', name: 'Coin', value: 1, tradeable: true)
+
+t1 = demo.transferables.find_by(identifier: 'dark_magician') || demo.transferables.create!(identifier: 'dark_magician', name: 'Dark Magician', value: 100, properties: { atk: 2500, def: 2100 })
+t2 = demo.transferables.find_by(identifier: 'blue_eye_dragon') || demo.transferables.create!(identifier: 'blue_eye_dragon', name: 'Blue-Eyes White Dragon', value: 200, properties: { atk: 3000, def: 2500 })
