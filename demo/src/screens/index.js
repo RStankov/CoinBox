21b5d56c18e78b8b79da65b18b72480950f77dd0
Radoslav Stankov <rstankov@gmail.com>
2018-04-01 02:43:41 +0300

    Remove placeholder screen

--- demo/src/screens/index.js
+++ demo/src/screens/index.js
@@ -1,11 +1,9 @@
 import { StackNavigator } from 'react-navigation';
 
 import Home from './Home';
-import PizzaPicker from './PizzaPicker';
 import SignIn from './SignIn';
 
 export default StackNavigator({
   Home: { screen: Home },
-  PizzaPicker: { screen: PizzaPicker },
   SignIn: { screen: SignIn },
 });
