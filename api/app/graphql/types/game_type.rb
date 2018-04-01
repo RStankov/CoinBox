87f9ca6cf307f94314d31db31f5e12e3915319e7
Radoslav Stankov <rstankov@gmail.com>
2018-04-01 10:18:54 +0300

    Use proper type

--- api/app/graphql/types/game_type.rb
+++ api/app/graphql/types/game_type.rb
@@ -1,7 +1,7 @@
 Types::GameType = GraphQL::ObjectType.define do
   name 'Game'
 
-  field :id, !types.String
+  field :id, !types.ID
   field :name, !types.String
 
   connection :store, Types::TransferableType.connection_type, function: Resolvers::StoreResolver
