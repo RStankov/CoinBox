411a6849e457ccb0c62687396441557114b5fb04
Radoslav Stankov <rstankov@gmail.com>
2018-04-01 05:49:13 +0300

    Make items purchasable

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -13,8 +13,8 @@ api_key = demo.api_keys.find_by(name: 'iOS') || demo.api_keys.create!(name: 'iOS
 c1 = demo.consumables.find_by(identifier: 'exp') || demo.consumables.create!(identifier: 'exp', name: 'Expirience', value: 0, tradeable: false)
 c2 = demo.consumables.find_by(identifier: 'coin') || demo.consumables.create!(identifier: 'coin', name: 'Coin', value: 1, tradeable: true, primary: true)
 
-t1 = demo.transferables.find_by(identifier: 'dark_magician') || demo.transferables.create!(identifier: 'dark_magician', name: 'Dark Magician', value: 100, properties: { atk: 2500, def: 2100 })
-t2 = demo.transferables.find_by(identifier: 'blue_eye_dragon') || demo.transferables.create!(identifier: 'blue_eye_dragon', name: 'Blue-Eyes White Dragon', value: 200, properties: { atk: 3000, def: 2500 })
+t1 = demo.transferables.find_by(identifier: 'dark_magician') || demo.transferables.create!(identifier: 'dark_magician', name: 'Dark Magician', value: 100, properties: { atk: 2500, def: 2100 }, purchasable: true)
+t2 = demo.transferables.find_by(identifier: 'blue_eye_dragon') || demo.transferables.create!(identifier: 'blue_eye_dragon', name: 'Blue-Eyes White Dragon', value: 200, properties: { atk: 3000, def: 2500 }, purchasable: true)
 
 player = demo.players.find_by_email('coinbox@example.com') || demo.players.create!(
   username: 'TwoThirds', email: 'coinbox@example.com', password: '123456789'
